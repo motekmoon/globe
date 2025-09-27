84b386691b0ff7c6c2c91e66756fb937131ea8fa
motekmoon <137325999+motekmoon@users.noreply.github.com>
2025-09-27 10:49:53 -0700

    fix: move LocationManager outside rotating group
    - Moved LocationManager outside the rotating group
    - Lines should now be visible and not affected by globe rotation
    - This should fix the line visibility issue
    - Globe still rotates but lines remain stable

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -26,20 +26,22 @@ const Globe: React.FC<GlobeProps> = ({ locations }) => {
   });
 
   return (
-    <group ref={groupRef}>
-      <mesh>
-        <sphereGeometry args={[2, 64, 32]} />
-        <meshPhongMaterial
-          map={worldMapTexture}
-          color="#ffffff"
-          shininess={50}
-          transparent={false}
-          emissive="#333333"
-          emissiveIntensity={0.3}
-        />
-      </mesh>
+    <>
+      <group ref={groupRef}>
+        <mesh>
+          <sphereGeometry args={[2, 64, 32]} />
+          <meshPhongMaterial
+            map={worldMapTexture}
+            color="#ffffff"
+            shininess={50}
+            transparent={false}
+            emissive="#333333"
+            emissiveIntensity={0.3}
+          />
+        </mesh>
+      </group>
       <LocationManager locations={locations} />
-    </group>
+    </>
   );
 };
 
